e471b15d26836d1f84742c43f9167e48a449c03c
halilibrahimsaltas <h.ibrahimsaltas@gmail.com>
2025-07-31 19:31:53 +0300

    Orders module added.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { ProductsModule } from './products/products.module';
     }),
     UsersModule,
     ProductsModule,
+    OrdersModule,
   ],
 })
 export class AppModule {}
